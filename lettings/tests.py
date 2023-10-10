d69977ee55bf239ecbdbfb4de3376da979674e08
franfif <francis.barrow@gmail.com>
2023-10-10 17:19:01 -0400

    update lettings index url name

--- lettings/tests.py
+++ lettings/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class LettingIndexViewTest(TestCase):
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse("lettings:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "lettings/index.html")
+
+    def test_mytest(self):
+        self.assertTrue(True)
