7ac50c79b57f17e4ad870110ef610f0f3c8cf337
franfif <francis.barrow@gmail.com>
2023-10-24 15:45:43 -0400

    comment and complete trigger_error view for Sentry

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -4,8 +4,10 @@ from django.urls import path, include
 from . import views
 
 
+# Trigger an uncaught error, used to test Sentry error logging
 def trigger_error(request):
     division_by_zero = 1 / 0
+    return division_by_zero
 
 
 urlpatterns = [
@@ -13,5 +15,6 @@ urlpatterns = [
     path("lettings/", include("lettings.urls")),
     path("profiles/", include("profiles.urls")),
     path("admin/", admin.site.urls),
+    # path to call the function view that will trigger an uncaught error
     path("sentry_debug/", trigger_error),
 ]
